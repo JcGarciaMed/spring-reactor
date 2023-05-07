29cb732e1762a4ab38d89ce7e3a6c4d0475a7035
Jorge García <jcgarciamed@gmail.com>
2023-05-07 00:33:47 -0500

    Update Controller

--- src/main/java/com/greymatter/springreactor/controller/PlatoController.java
+++ src/main/java/com/greymatter/springreactor/controller/PlatoController.java
@@ -2,9 +2,17 @@ package com.greymatter.springreactor.controller;
 
 import com.greymatter.springreactor.model.Plato;
 import com.greymatter.springreactor.service.PlatoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/platos")
@@ -17,28 +25,51 @@ public class PlatoController {
     }
 
     @GetMapping
-    public Flux<Plato> listar(){
-        return service.listar();
+    public Mono<ResponseEntity<Flux<Plato>>> listar(){
+        return Mono.just(ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.listar()));
     }
 
     @GetMapping("/{id}")
-    public Mono<Plato> listarPorId(@PathVariable("id") String id){
-        return service.listarPorId(id);
+    public Mono<ResponseEntity<Plato>> listarPorId(@PathVariable("id") String id){
+        return service.listarPorId(id)
+                .map(plato -> ResponseEntity
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(plato));
     }
 
     @PostMapping
-    public Mono<Plato> registrar(@RequestBody Plato plato){
-        return service.registrar(plato);
+    public Mono<ResponseEntity<Plato>> registrar(@RequestBody Plato plato, final ServerHttpRequest request){
+        return service.registrar(plato)
+                .map(element -> ResponseEntity
+                        .created(URI.create(request.getURI().toString().concat("/").concat(element.getId())))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(element));
     }
 
     @PutMapping
-    public Mono<Plato> modificar(@RequestBody Plato plato){
-        return service.modificar(plato);
+    public Mono<ResponseEntity<Plato>> modificar(@RequestBody Plato plato){
+        return service.listarPorId(plato.getId())
+                .flatMap(element -> service.modificar(plato))
+                .map(element -> ResponseEntity
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(element))
+                .defaultIfEmpty(new ResponseEntity<Plato>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/{id}")
-    public Mono<Void> eliminar(@PathVariable("id") String id){
-        return service.eliminar(id);
+    public Mono<ResponseEntity<Void>> eliminar(@PathVariable("id") String id){
+        return service.listarPorId(id)
+                .flatMap(plato -> {
+                    return service.eliminar(id)
+                            //.then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
+                            .thenReturn(new ResponseEntity<Void>(HttpStatus.NO_CONTENT));
+                })
+                .defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
     }
 
     
